3a383b5f06ccc76dae98b3e5957a2089d9faaf2c
Dayana Alonso <dayanaalonso@dayanas-mbp.home>
2019-08-08 14:42:49 +0100

    adding 7 actions 4 Verbs

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,10 +1,33 @@
 class SchoolsController < ApplicationController
-  def new
-    end
+  #Get
+  def index
+  end
 
+  #Get
+  def show
+  end
+  
+  #Get
   def new
     @school = School.new
   end
+
+  #Post
+  def create
+  end
+  
+  #Get
+  def edit
+  end
+
+  #Patch/Put
+  def update
+  end
+
+  #Detele
+  def destroy
+  end
+
 end
 
 
